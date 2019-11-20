f8e556186eef4fca1369189a950bb3ae0c20a5cf
kriptonuz <52530659+kriptonuz@users.noreply.github.com>
2019-11-20 10:55:22 +0500

    Add phpUnit

---
+++ tests/SapiEmitterTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Cekta\HTTP\Server\Test;
+
+use Cekta\HTTP\Server\SapiEmitter;
+use PHPUnit\Framework\TestCase;
+use TypeError;
+
+class SapiEmitterTest extends TestCase
+{
+    public function testInvalidResponse()
+    {
+        $this->expectException(TypeError::class);
+        $emitter = new SapiEmitter();
+        $emitter->emit('invalid responce');
+    }
+}
